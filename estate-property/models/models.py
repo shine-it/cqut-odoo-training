ea9981a159c661e751c85f5beea587d69fbc3507
Tony Gu <tony@openerp.cn>
2020-12-19 16:03:47 +0800

    added ep table

--- estate-property/models/models.py
+++ estate-property/models/models.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class estate-property(models.Model):
-#     _name = 'estate-property.estate-property'
-#     _description = 'estate-property.estate-property'
+class EstateProperty(models.Model):
+    _name = 'estate-property.ep'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    date = fields.Date()
+    description = fields.Text()
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     for record in self:
+    #         record.value2 = float(record.value) / 100
